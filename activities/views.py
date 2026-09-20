d05fac4ddbffecf64c3c3d688be6e6d3e7df2b3f
henry <henryjr2@illinois.edu>
2026-09-20 14:21:06 -0500

    friendlist view

--- activities/views.py
+++ activities/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from activities.models import Friends
+from django.shortcuts import get_object_or_404, render
+from django.views import View
+from django.views.generic import ListView
 
-# Create your views here.
+def friends_list(request):
+    friends = Friends.objects.all()
+    return render(request, "friend_list.html", {"friends": friends})
